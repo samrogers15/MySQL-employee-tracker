563b202e029cff9576254ea95bb9bfbda553adc3
Sam Rogers <samrogers15@gmail.com>
2021-02-17 10:39:29 -0800

    updating with npm dotenv compatibility

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
 const cTable = require('console.table');
+require('dotenv').config();
 
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: '',
+    password: process.env.password,
     database: 'employeeTrackerDB',
 });
 
